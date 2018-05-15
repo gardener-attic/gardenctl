916248d2c485df78633e22f5273c632c28413d44
Sebastian Stauch <sebastian.stauch@sap.com>
2018-05-15 04:29:07 +0200

    added register and unregister for operatorshift

---
+++ cmd/register.go
@@ -0,0 +1,48 @@
+// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// registerCmd represents the register command
+var registerCmd = &cobra.Command{
+	Use:   "register (e-mail)",
+	Short: "Register as cluster admin for the operator shift",
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) != 1 {
+			fmt.Println("Command must be in the format: register (e-mail)")
+			os.Exit(2)
+		}
+		err := ExecCmd("kubectl set subject clusterrolebinding garden-administrators --user="+args[0], false, "KUBECONFIG="+getGardenKubeConfig())
+		checkError(err)
+	},
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// registerCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// registerCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
