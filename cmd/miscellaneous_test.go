acb0f725632867345b6dc89495b0682af92127a1
Sebastian Stauch <sebastian.stauch@sap.com>
2018-08-15 14:33:32 +0200

    adpat test

--- cmd/miscellaneous_test.go
+++ cmd/miscellaneous_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 var _ = Describe("Miscellaneous", func() {
-	var gardenClusters GardenClusters
+	var gardenConf GardenConfig
 	var target Target
 	dumpPath := "/tmp"
 	pathTarget := dumpPath + "/target2"
@@ -19,14 +19,15 @@ var _ = Describe("Miscellaneous", func() {
 	if err != nil {
 		panic(err)
 	}
-	gardenConfig := `
+	gConfig := `
+githubURL: https://github.croft.gardener.corp
 gardenClusters:
 - name: dev
   kubeConfig: /tmp/kubeconfig.yaml
 - name: prod
   kubeConfig: /tmp/kubeconfig.yaml
 `
-	content := []byte(gardenConfig)
+	content := []byte(gConfig)
 	file.Write(content)
 	err = file.Close()
 	if err != nil {
@@ -51,9 +52,9 @@ gardenClusters:
 	})
 	Context("After calling GetGardenClusters", func() {
 		It("GardenCluster Name should be dev ", func() {
-			GetGardenClusters(pathGardenConfig, &gardenClusters)
-			Expect(gardenClusters.GardenClusters[0].Name).To(Equal("dev"))
-			Expect(gardenClusters.GardenClusters[1].Name).To(Equal("prod"))
+			GetGardenConfig(pathGardenConfig, &gardenConf)
+			Expect(gardenConf.GardenClusters[0].Name).To(Equal("dev"))
+			Expect(gardenConf.GardenClusters[1].Name).To(Equal("prod"))
 		})
 	})
 
