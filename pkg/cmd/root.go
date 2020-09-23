c9dfc133afa3cc40c1f05f87e0ae765f04681284
Neo Liang <neo.liang@sap.com>
2020-09-23 11:08:02 +0800

    Revert "list orphaned resources"

--- pkg/cmd/root.go
+++ pkg/cmd/root.go
@@ -149,7 +149,6 @@ func init() {
 	RootCmd.AddCommand(NewKubectlCmd(), NewKaCmd(), NewKsCmd(), NewKgCmd(), NewKnCmd())
 	RootCmd.AddCommand(NewKubectxCmd())
 	RootCmd.AddCommand(NewTerraformCmd(targetReader))
-	RootCmd.AddCommand(NewInfraCmd(targetReader))
 	RootCmd.AddCommand(NewAliyunCmd(targetReader), NewAwsCmd(targetReader), NewAzCmd(targetReader), NewGcloudCmd(targetReader), NewOpenstackCmd(targetReader))
 	RootCmd.AddCommand(NewInfoCmd(targetReader, ioStreams))
 	RootCmd.AddCommand(NewVersionCmd(), NewUpdateCheckCmd())
