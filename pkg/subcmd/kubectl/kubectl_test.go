8b2b16a693b7ed4359af11468befeea522884d05
Andrei Panov <andrei.panov@sap.com>
2020-11-03 16:02:16 +0100

    final tests

--- pkg/subcmd/kubectl/kubectl_test.go
+++ pkg/subcmd/kubectl/kubectl_test.go
@@ -1,11 +1,43 @@
 package kubectl
 
 import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 	"regexp"
 	"strings"
 	"testing"
 )
 
+var _ = Describe("Build kubectl commands", func() {
+
+	Context("kubectl commands", func() {
+
+		It("should build kubectl command", func() {
+
+			expected := "logs --kubeconfig=/path/to/configfile myPodmyContainer -n myns --tail=200 --since=3e-07s"
+			command := BuildKubectlCommandArgs("/path/to/configfile", "myns", "myPod", "myContainer", 200, 300)
+			join := strings.Join(command, " ")
+			Expect(expected).To(Equal(join))
+		})
+
+		It("should build kubectl command", func() {
+
+			//test `normalizeTimestamp` first - replace timestamp with some predefined values
+			expected := `--kubeconfig=/path/to/configfile exec loki-0 -n myns -- wget 'http://localhost:3100/loki/api/v1/query_range' -O- --post-data='query={pod_name=~"nginx-pod.*"}&&query={container_name=~"mycontainer.*"&&limit=200&&start=1603184413805314000&&end=1604394013805314000'`
+			expectedNorm := `--kubeconfig=/path/to/configfile exec loki-0 -n myns -- wget 'http://localhost:3100/loki/api/v1/query_range' -O- --post-data='query={pod_name=~"nginx-pod.*"}&&query={container_name=~"mycontainer.*"&&limit=200&&start=101010&&end=202020'`
+			norm := normalizeTimestamp(expected)
+			Expect(expectedNorm).To(Equal(norm))
+
+			//test real command builder
+			args := BuildLokiCommandArgs("/path/to/configfile", "myns", "nginx-pod", "mycontainer", 200, 0)
+			command := strings.Join(args, " ")
+			normCommand := normalizeTimestamp(command)
+			Expect(expectedNorm).To(Equal(normCommand))
+		})
+	})
+})
+
+//all these tests will be deleted after the review except `normalizeTimestamp`
 func Test_buildKubectlCommand(t *testing.T) {
 	KUBECONFIG := "/path/to/configfile"
 	command := BuildKubectlCommand(KUBECONFIG, "myns", "myPod", "myContainer", 200, 0)
